0655260eb05ec693703a0ba7f6d9a1bc843fa77a
Aditya Sridhar <aditya@justworks.com>
2019-08-03 15:11:16 -0400

    fix: typo in print command to allow for string interpolation.

--- print_pop.py
+++ print_pop.py
@@ -8,4 +8,4 @@ def send_to_printer(file_path):
 def fetch_files_from_s3():
 	print('fetching files from s3')
 
-send_to_printer('/home/pi/Downloads/A4 (1).pdf')
+send_to_printer(f'/home/pi/Downloads/A4 (1).pdf')
