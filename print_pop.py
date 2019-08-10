c8e37c03303bbaf6ee30da41124d0239d8671257
Aditya Sridhar <adityas@live.com>
2019-08-10 14:21:42 -0400

    feat: function that loops through staged files and sends them to the printer.

--- print_pop.py
+++ print_pop.py
@@ -9,6 +9,12 @@ def send_to_printer(file_path):
 	system_command = 'lp "%s"'%(file_path)
 	os.system(system_command)
 
+def print_files_to_print():
+	directory = '/home/pi/' + TO_PRINT_FILES_BUCKET
+	for filename in os.listdir(directory):
+		send_to_printer(directory + '/' + filename)
+		print('Sent the following file to printer: ' + filename)
+
 def fetch_files_from_s3():
 	print('fetching files from s3')
 
@@ -28,7 +34,9 @@ def download_files_to_print():
 	    if len(filename) == 0:
             	continue
 	    s3_bucket.download_file(i.key, '/home/pi/' + TO_PRINT_FILES_BUCKET + '/' + filename)
+	    print('Finished downloading: ' + filename)
 
 
 # send_to_printer('/home/pi/Downloads/A4 (1).pdf')
 download_files_to_print()
+print_files_to_print()
